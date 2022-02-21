bd30ae7be26fda808e9ec4b5665935a9e12bd3d0
hnee <rornfdlek@naver.com>
2022-02-21 16:04:55 +0900

    Movie Details

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,36 @@
+import Link from "next/link";
 import Head from "next/head"
 import { useEffect, useState } from "react"
 import Seo from "../components/Seo"
+import { useRouter } from "next/router";
 
 
 export default function Home({results}) {
+  const router = useRouter();
+  const onClick = (id, title) => {
+    router.push({
+      pathname: `movies/${id}`,
+      query: {
+        title,
+      }
+    },`movies/${id}`);
+  }
   return (
     <div className="container">
       <Seo title="Home" />
       {results?.map((movie) => (
-        <div className="movie" key={movie.id}>
-          <img src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} />
-          <h4>{movie.original_title}</h4>
+        <div onClick={() => onClick(movie.id, movie.original_title)} className="movie" key={movie.id}>
+          <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} />
+          <h4>
+            <Link href={{
+              pathname: `movies/${movie.id}`,
+              query: {
+                title: movie.original_title
+              }
+            }} as={`movies/${movie.id}`}>
+              <a>{movie.original_title}</a>
+            </Link>
+          </h4>
         </div>
       ))}
       <style jsx>{`
@@ -19,6 +39,9 @@ export default function Home({results}) {
           grid-template-columns: 1fr 1fr;
           padding: 20px;
           gap: 20px;
+        }
+        .movie {
+          cursor: pointer;
         }
         .movie img {
           max-width: 100%;
